83a63f724a7c3c5a65685bb8b31ae941ebc1e24d
Jessica Lair <jessica.lair@Elevates-MacBook-Pro.local>
2020-05-03 14:54:52 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from 'react';
+import NavBar from "./homePage/navBar";
+import Submissions from "./submissionsPage/submissions";
+import Hero from "./homePage/hero";
+import SubmissionForm from "./homePage/formsubmission";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <NavBar />
+    <Router>
+      <Route exact path="/submit" component={Submissions} />
+      <Route
+        exact
+        path="/"
+        component={() => {
+          return (
+            <React.Fragment>
+              <Hero />
+              <hr></hr>
+              <SubmissionForm />
+              <hr></hr>
+              
+              <p>The current time is {currentTime}.</p>
+            </React.Fragment>
+          );
+        }}
+      />
+    </Router>
+  </div>
   );
 }
 
 export default App;
+
